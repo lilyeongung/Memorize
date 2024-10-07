53f3d61b784c021be37af9af263652c3ff2c0716
Andrew Park <ayepark@icloud.com>
2024-10-07 12:25:42 -0700

    Initial Commit CardView isFaceUp

---
+++ Memorize/MemorizeGameView.swift
@@ -0,0 +1,18 @@
+//
+//  MemorizeGameView.swift
+//  Memorize
+//
+//  Created by Andrew Park on 10/7/24.
+//
+
+import SwiftUI
+
+struct MemorizeGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemorizeGameView()
+}
